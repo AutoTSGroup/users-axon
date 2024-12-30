b85067c32a7c1c6a4ea7c18f848f259e51ae4d47
Timpohey7 <timopheyonisenko@gmail.com>
2024-12-30 14:25:03 +0300

    add workflow

--- src/main/java/com/autotradingsystem/users/query/api/data/repository/UserMappedBrokerRepository.java
+++ src/main/java/com/autotradingsystem/users/query/api/data/repository/UserMappedBrokerRepository.java
@@ -4,7 +4,6 @@ import com.autotradingsystem.users.query.api.data.models.UserMappingBroker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface UserMappedBrokerRepository extends JpaRepository<UserMappingBroker, UUID> {
